65a8984d057219467a4a5a7f62a369c80a3d101d
Sam Davies <seivadmas@gmail.com>
2016-04-09 13:40:35 +0800

    Add questionaire

--- app/controllers/employee_card_controller.rb
+++ app/controllers/employee_card_controller.rb
@@ -12,6 +12,29 @@ class EmployeeCardController < ApplicationController
   end
 
   def mental
+    @questions = [
+      "I found it hard to  wind down",
+      "I was aware of dryness of my mouth",
+      "I couldn't seem to experience any positive feeling at all",
+      "I experienced breathing difficulty",
+      "I found it difficult to work up the initiative to do things",
+      "I tended to over-react to situations",
+      "I experienced trembling (eg, in the hands)",
+      "I felt that I was using a lot of nervous energy",
+      "I found myself in situations that made me so anxious I was most relieved when they ended",
+      "I felt that I had nothing to look forward to",
+      "I found myself getting agitated",
+      "I found it difficult to relax",
+      "I felt down-hearted and blue",
+      "I was intolerant of anything that kept me from getting on with what I was doing",
+      "I felt I was close to panic",
+      "I was unable to become enthusiastic about anything",
+      "I felt I wasn't worth much as a person",
+      "I felt that I was rather touchy",
+      "I was aware of the action of my heart in the absence of physical exertion",
+      "I felt scared without any good reason",
+      "I felt that life wasn't worthwhile",
+    ]
   end
 
   def aod
